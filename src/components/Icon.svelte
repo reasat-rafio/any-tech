2949b3138870c8c17731bffca12265e8a1eb4c2f
reasat-rafio <rafioar96@gmail.com>
2023-05-09 15:03:25 +0600

    :art: icon component added for astro and svelte

---
+++ src/components/Icon.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  // update vaiant here
+  export let icon: 'ChevronRight';
+  export let airaLabel = 'icon';
+  export let className = '';
+</script>
+
+<object
+  class={className}
+  aria-label={airaLabel}
+  type="image/svg+xml"
+  data={`/icons/${icon}.svg`}
+  >Your borwser doesn't support SVG
+</object>
