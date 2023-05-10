542bee1bb6cb5d91180475ec055dd603c91da857
reasat-rafio <rafioar96@gmail.com>
2023-05-10 17:44:20 +0600

    :construction: banking tech flow component layout created

---
+++ src/components/pages/home/banking/flow/IntegratedTechnologyFlow.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import type { IntegratedTechnologyFlow } from '@/lib/types/homePage';
+  import Technology from './Technology.svelte';
+  export let integratedTechnologyFlow: IntegratedTechnologyFlow[];
+
+  let [firstTechnology, secondTechnology, ...restTechnologies] =
+    integratedTechnologyFlow;
+</script>
+
+<section class="flex flex-col space-y-[19px] text-center">
+  <div class="px-[78px] py-[29px] bg-[#FAFAFA] rounded-[36px]">
+    {#if !!firstTechnology}
+      <Technology
+        name={firstTechnology.name}
+        platform={firstTechnology.platform}
+        service={firstTechnology.service}
+      />
+    {/if}
+  </div>
+
+  <div
+    class="flex-1 bg-[#FAFAFA] rounded-[36px] px-[78px] py-[29px] flex flex-col justify-center items-center w-full space-y-[40px]"
+  >
+    {#if secondTechnology}
+      <Technology
+        name={secondTechnology.name}
+        platform={secondTechnology.platform}
+        service={secondTechnology.service}
+      />
+    {/if}
+
+    <div class="flex w-full space-x-[15%]">
+      {#each restTechnologies as tech}
+        <Technology
+          name={tech.name}
+          platform={tech.platform}
+          service={tech.service}
+        />
+      {/each}
+    </div>
+  </div>
+</section>
---
+++ src/components/pages/home/banking/flow/Technology.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let name: string;
+  export let platform: string;
+  export let service: string;
+</script>
+
+<div class="rounded-[12px]">
+  <div
+    class="bg-white py-[21px] px-[24px] rounded-sm flex flex-col shadow-card"
+  >
+    <span class="text-blue-primary as text-[12px] font-bold tracking-[0.16em]">
+      {name}
+    </span>
+    <span class="text-blue-secondary text-[24px] font-Montserrat font-semibold">
+      {platform}
+    </span>
+  </div>
+  <p class="text-blue-crystal text-[12px] font-semibold mt-[23px]">
+    {service}
+  </p>
+</div>
