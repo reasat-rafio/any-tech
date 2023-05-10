d11c772f16db30ac2c9a505fe5a86ee250d24b83
reasat-rafio <rafioar96@gmail.com>
2023-05-10 18:02:39 +0600

    :iphone: flow diagram positioning and responsiveness update

--- src/components/pages/home/banking/flow/IntegratedTechnologyFlow.svelte
+++ src/components/pages/home/banking/flow/IntegratedTechnologyFlow.svelte
@@ -7,8 +7,10 @@
     integratedTechnologyFlow;
 </script>
 
-<section class="flex flex-col space-y-[19px] text-center">
-  <div class="px-[78px] py-[29px] bg-[#FAFAFA] rounded-[36px]">
+<section class="{$$props.class} flex flex-col space-y-[19px] text-center">
+  <div
+    class="py-[26px] px-[20px] lg:px-[78px] lg:py-[29px] bg-[#FAFAFA] lg:rounded-[36px] rounded-[16px]"
+  >
     {#if !!firstTechnology}
       <Technology
         name={firstTechnology.name}
@@ -19,7 +21,7 @@
   </div>
 
   <div
-    class="flex-1 bg-[#FAFAFA] rounded-[36px] px-[78px] py-[29px] flex flex-col justify-center items-center w-full space-y-[40px]"
+    class="flex-1 bg-[#FAFAFA] lg:rounded-[36px] rounded-[16px] py-[26px] px-[20px] lg:px-[78px] lg:py-[29px] flex flex-col justify-center items-center w-full lg:space-y-[40px] space-y-[20px]"
   >
     {#if secondTechnology}
       <Technology
@@ -29,7 +31,7 @@
       />
     {/if}
 
-    <div class="flex w-full space-x-[15%]">
+    <div class="flex w-full space-x-[15%] justify-around">
       {#each restTechnologies as tech}
         <Technology
           name={tech.name}
--- src/components/pages/home/banking/flow/Technology.svelte
+++ src/components/pages/home/banking/flow/Technology.svelte
@@ -4,9 +4,9 @@
   export let service: string;
 </script>
 
-<div class="rounded-[12px]">
+<div class="lg:rounded-[12px] rounded-[8px]">
   <div
-    class="bg-white py-[21px] px-[24px] rounded-sm flex flex-col shadow-card"
+    class="bg-white lg:py-[21px] py-[13px] lg:px-[24px] px-[16px] rounded-sm flex flex-col shadow-card"
   >
     <span class="text-blue-primary as text-[12px] font-bold tracking-[0.16em]">
       {name}
@@ -15,7 +15,9 @@
       {platform}
     </span>
   </div>
-  <p class="text-blue-crystal text-[12px] font-semibold mt-[23px]">
+  <p
+    class="text-blue-crystal lg:text-[12px] text-[8.29px] font-semibold mt-[23px]"
+  >
     {service}
   </p>
 </div>
