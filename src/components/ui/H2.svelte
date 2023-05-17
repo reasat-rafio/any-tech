a16ae5b570f32565362c1f2966e949b4fe8ad3b2
reasat-rafio <rafioar96@gmail.com>
2023-05-18 00:09:37 +0600

    :wrench: headers component and tailwind config update

--- src/components/ui/H2.svelte
+++ src/components/ui/H2.svelte
@@ -1,5 +1,5 @@
 <h2
-  class="whitespace-pre-line font-Montserrat text-res-head-2 text-black lg:text-head-2"
+  class="whitespace-pre-line font-Montserrat text-res-head-2 text-black md:text-tab-head-2 xl:text-head-2"
 >
   <slot />
 </h2>
