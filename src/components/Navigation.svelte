629f19fe91dc9b12578c46847e4bd1e835988906
Mahabubur Rahman <rony@otterdev.io>
2023-05-18 07:46:58 +0600

    fix navbar touching sides

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -11,7 +11,7 @@
 <div
   bind:clientHeight={$navbarHeight}
   id="main-navigation"
-  class="max-w-[1200px] mx-auto relative flex items-center flex-wrap justify-between lg:relative z-[9999] max-lg:py-8 max-lg:container"
+  class="max-w-[1200px] mx-auto relative flex items-center flex-wrap justify-between lg:relative z-[9999] max-lg:py-8 max-xl:container"
 >
   <h1>
     <a aria-label="Go to home page" href="/" rel="home"
