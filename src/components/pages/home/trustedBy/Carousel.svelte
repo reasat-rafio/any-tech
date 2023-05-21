8ab5b432d58a9003b33add7537973667f9ee566e
reasat-rafio <rafioar96@gmail.com>
2023-05-21 20:53:45 +0600

    :recycle: trusted by carousel converted to swiper carousel

--- src/components/pages/home/trustedBy/Carousel.svelte
+++ src/components/pages/home/trustedBy/Carousel.svelte
@@ -1,36 +1,36 @@
 <script lang="ts">
-  import { Splide, SplideSlide } from '@splidejs/svelte-splide';
-  import '@splidejs/svelte-splide/css';
   import { imageBuilder } from '@/lib/helpers';
   import SanityImage from '@/lib/sanity-image/sanity-image.svelte';
+  import { Autoplay } from 'swiper';
+  import 'swiper/css';
+  import 'swiper/css/autoplay';
+  import { Swiper, SwiperSlide } from 'swiper/svelte';
 
   export let logos: SanityTitledImage[];
 </script>
 
 <div class={$$props.class}>
   <div class="w-[140%] -translate-x-[15%]">
-    <Splide
-      options={{
-        arrows: false,
-        pagination: false,
-        autoplay: true,
-        speed: 1500,
-        type: 'loop',
-        gap: '2rem',
-        perPage: 3,
-        perMove: 1,
-      }}
+    <Swiper
+      slidesPerView={3}
+      modules={[Autoplay]}
+      loop
+      spaceBetween={32}
+      grabCursor
+      mousewheel={{ forceToAxis: true }}
+      autoplay
+      speed={1500}
       aria-label="Company Logos"
     >
       {#each logos as logo}
-        <SplideSlide class="flex items-center">
+        <SwiperSlide class="flex items-center my-auto">
           <SanityImage
             imageUrlBuilder={imageBuilder}
             src={logo}
             sizes="170px"
           />
-        </SplideSlide>
+        </SwiperSlide>
       {/each}
-    </Splide>
+    </Swiper>
   </div>
 </div>
