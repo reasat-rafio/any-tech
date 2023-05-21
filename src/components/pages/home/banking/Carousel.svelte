b7ad6be96ecfc394c6c4406d1daa9748cb3c4962
reasat-rafio <rafioar96@gmail.com>
2023-05-21 10:53:57 +0600

    :art: carousel layout update

--- src/components/pages/home/banking/Carousel.svelte
+++ src/components/pages/home/banking/Carousel.svelte
@@ -16,46 +16,60 @@
 
 <div class={$$props.class}>
   <Splide
-    class=""
+    class="mt-20"
     options={{
       arrows: false,
       pagination: false,
       autoplay: true,
       speed: 1500,
-      gap: '1rem',
+      gap: '2rem',
     }}
     aria-label="Integrated Technologies"
   >
     {#each solutions as { name, description, platform, link, image }}
       <SplideSlide class="pb-6 px-2">
         <article
-          class="p-[24px] space-y-[24px] shadow-card-light-sm rounded-lg"
+          class="p-[24px] lg:px-[38px] lg:gap-[40px] lg:py-[43px] xl:px-[48px] xl:gap-[60px] xl:py-[53px] shadow-card-light-sm lg:shadow-card-light rounded-lg grid lg:grid-cols-2 grid-cols-1"
         >
-          {#if name}
-            <H5>{name}</H5>
-          {/if}
-          <H3>{platform}</H3>
-          <div class="rounded-[8px] w-full min-h-[125px] overflow-hidden">
+          <section class="space-y-[24px] lg:space-y-[26px]">
+            {#if name}
+              <H5>{name}</H5>
+            {/if}
+            <H3>{platform}</H3>
+            <div
+              class="rounded-[8px] w-full min-h-[125px] overflow-hidden lg:hidden"
+            >
+              <SanityImage
+                imageUrlBuilder={imageBuilder}
+                class="h-full w-full object-cover max-h-[400px]"
+                src={image}
+                sizes="200px"
+              />
+            </div>
+            <Description class="whitespace-pre-line">
+              <PortableText
+                components={{
+                  block: { normal: CustomParagraph },
+                }}
+                value={description}
+              />
+            </Description>
+            {#if !!link?.title && !!link?.href}
+              <Link href={link.href}>
+                {link.title}
+              </Link>
+            {/if}
+          </section>
+          <div
+            class="rounded-md w-full min-h-[220px] overflow-hidden lg:block hidden"
+          >
             <SanityImage
               imageUrlBuilder={imageBuilder}
-              class="h-full w-full object-cover"
+              class="h-full w-full object-cover aspect-square xl:min-h-[450px]"
               src={image}
-              sizes="200px"
+              sizes="500px"
             />
           </div>
-          <Description class="whitespace-pre-line">
-            <PortableText
-              components={{
-                block: { normal: CustomParagraph },
-              }}
-              value={description}
-            />
-          </Description>
-          {#if !!link?.title}
-            <Link href={link?.href}>
-              {link.title}
-            </Link>
-          {/if}
         </article>
       </SplideSlide>
     {/each}
