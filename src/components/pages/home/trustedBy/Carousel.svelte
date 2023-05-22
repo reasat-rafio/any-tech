2adc3a1eb8fa4679d9180dfac37b2897462aa817
Mahabubur Rahman <rony@otterdev.io>
2023-05-22 07:12:55 +0600

    improve trusted by mobile carousel

--- src/components/pages/home/trustedBy/Carousel.svelte
+++ src/components/pages/home/trustedBy/Carousel.svelte
@@ -10,27 +10,23 @@
 </script>
 
 <div class={$$props.class}>
-  <div class="w-[140%] -translate-x-[15%]">
-    <Swiper
-      slidesPerView={3}
-      modules={[Autoplay]}
-      loop
-      spaceBetween={32}
-      grabCursor
-      mousewheel={{ forceToAxis: true }}
-      autoplay
-      speed={1500}
-      aria-label="Company Logos"
-    >
-      {#each logos as logo}
-        <SwiperSlide class="flex items-center my-auto">
-          <SanityImage
-            imageUrlBuilder={imageBuilder}
-            src={logo}
-            sizes="170px"
-          />
-        </SwiperSlide>
-      {/each}
-    </Swiper>
-  </div>
+  <Swiper
+    slidesPerView="auto"
+    loopedSlides={logos.length}
+    modules={[Autoplay]}
+    loop
+    spaceBetween={32}
+    grabCursor
+    mousewheel={{ forceToAxis: true }}
+    autoplay
+    speed={1500}
+    centeredSlides
+    aria-label="Company Logos"
+  >
+    {#each logos as logo}
+      <SwiperSlide class="flex items-center my-auto w-[170px]">
+        <SanityImage imageUrlBuilder={imageBuilder} src={logo} sizes="170px" />
+      </SwiperSlide>
+    {/each}
+  </Swiper>
 </div>
