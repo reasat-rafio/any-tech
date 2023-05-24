0bfc8a98cf1978e8013c8725af9df4ea9a0aa7cb
reasat-rafio <rafioar96@gmail.com>
2023-05-24 13:34:18 +0600

    :white_check_mark: our values section complete

---
+++ src/components/pages/about/values/Card.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import H5 from '@/components/ui/H5.svelte';
+  import { imageBuilder } from '@/lib/helpers';
+  import SanityImage from '@/lib/sanity-image/sanity-image.svelte';
+  import type { SanityAsset } from '@sanity/image-url/lib/types/types';
+
+  export let image: SanityAsset;
+  export let title: string;
+  export let description: string;
+</script>
+
+<li>
+  <div class="md:mb-[42px] mb-[26px]">
+    <SanityImage
+      imageUrlBuilder={imageBuilder}
+      src={image}
+      class="mx-auto h-[155px] w-[155px] overflow-hidden rounded-full border-[8.5px] border-white object-cover drop-shadow-ellipse"
+      sizes="170px"
+    />
+  </div>
+
+  <H5 class="mb-[16px]">{title}</H5>
+  <p class="md:text-[18px] text-[14px] leading-[160%] text-blue-secondary">
+    {description}
+  </p>
+</li>
--- src/lib/types/aboutPage.ts
+++ src/lib/types/aboutPage.ts
@@ -1,6 +1,23 @@
-type Section = CommonHeroData | CommonFeaturedContentData;
+import type { SanityAsset } from '@sanity/image-url/lib/types/types';
+
+type Section = CommonHeroData | CommonFeaturedContentData | ValuesData;
 
 export interface AboutPageData {
   seo: BaseMetaData;
   sections: Section[];
+}
+
+export interface ValuesData {
+  _key: string;
+  _type: 'aboutPage.values';
+  title?: string;
+  subtitle?: string;
+  values?: ValueData[];
+}
+
+export interface ValueData {
+  _key: string;
+  image: SanityAsset;
+  title: string;
+  description: string;
 }
